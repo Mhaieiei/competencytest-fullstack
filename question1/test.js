23939b79f3698bbbaeabb5e9f2b4e91a0baae596
Chalamphong Pandey <anshul@atvantage.co.th>
2021-07-08 14:34:50 +0700

    Add extra unit tests
    Add watch script

--- question1/test.js
+++ question1/test.js
@@ -13,6 +13,7 @@ test("Sum handles not number string as an input,  should throw error Cannot pars
 });
 
 test("Sum handle input string positive", () => {
+  expect(sum("0", "0")).toEqual("0");
   expect(sum("5", "5")).toEqual("10");
   expect(sum("2", "3")).toEqual("5");
   expect(sum("20", "30")).toEqual("50");
@@ -23,3 +24,26 @@ test("Sum handle input string negative", () => {
   expect(sum("-20", "-5")).toEqual("-25");
   expect(sum("-10", "-4")).toEqual("-14");
 });
+
+const validationTestCases = [
+  [[], {}, "Cannot parse string to number"],
+  [[], [], "Cannot parse string to number"],
+  [{}, [], "Cannot parse string to number"],
+  [{}, {}, "Cannot parse string to number"],
+  [0, [], "Cannot parse string to number"],
+  [[], 0, "Cannot parse string to number"],
+  [0, {}, "Cannot parse string to number"],
+  [{}, 0, "Cannot parse string to number"],
+  [true, false, "Cannot parse string to number"],
+  [false, true, "Cannot parse string to number"],
+  [true, true, "Cannot parse string to number"],
+  [false, false, "Cannot parse string to number"],
+  ["2 4", "2", "Cannot parse string to number"],
+  ["2", "2 4", "Cannot parse string to number"],
+];
+
+describe.each(validationTestCases)('Validating sum(%s, %s)', (param1, param2, errorMessage) => {
+  test(`throws ${errorMessage}`, () => {
+    expect(() => sum(param1, param2)).toThrowError(errorMessage);
+  });
+})
